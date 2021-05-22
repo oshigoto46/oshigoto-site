7c9146510d5ca568768cc47bef87ff9d95f8c1af
shiro <ppp.shiro46@gmail.com>
2021-05-22 18:38:56 +0000

    work ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Tabs
         selectedIndex={tabIndex}
         onSelect={(tabIndex) => setTabIndex(tabIndex)}
       >
         <TabList>
-          <Tab style={style}>会員紹介</Tab>
+          <Tab>会員紹介</Tab>
           <Tab>Title 2</Tab>
         </TabList>
         <TabPanel>t1t1t1</TabPanel>
