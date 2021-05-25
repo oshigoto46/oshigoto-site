cc24d6ca265ab7277257acb28fae7a68f4be0083
shiro <ppp.shiro46@gmail.com>
2021-05-25 14:07:18 +0000

    work ref

--- src/constants/routes.tsx
+++
@@ -1,6 +0,0 @@
-export const HOME = '/';
-export const SIGNIN = '/signin';
-export const SIGNUP = '/signup';
-export const SETTINGS = '/settings';
-export const USER_PROFILE = '/profile';
-export const RESET_PASSWORD = '/resetpassword';
