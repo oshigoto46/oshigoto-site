cc158343c8046a8204be0edda2b28f3b0714747a
shiro <ppp.shiro46@gmail.com>
2021-05-25 12:15:51 +0000

    https://github.com/oshigoto46/oshigoto-site/issues/4 work ref

--- src/component/Navbar/Header.tsx
+++ src/component/Navbar/Header.tsx
@@ -1,25 +1,23 @@
 import React, { useState } from 'react';
 import { Drawer, Button} from 'antd';
 import RightMenu from '../Right-Menu/RightMenu'
-import {UploadModal} from "./UploadModal";
+import {UploadModal} from "../Photos/UploadModal";
 import MenuIcon from '@material-ui/icons/Menu';
-//import HomeSharpIcon from "@material-ui/icons/HomeSharp";
+import IconButton from "@material-ui/core/IconButton";
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
 
-	const [isDrawerMenuVisible, setIsDrawerMenuVisible] = useState<boolean>(
-		false
-	  );
-    const showDrawer = (): void => {
-		setIsDrawerMenuVisible(true);
-	  };
-	
-    const onClose = (): void => {
-		setIsDrawerMenuVisible(false);
-	  };
-	const [open, setOpen] = useState(true);
-	const handleClose = () => setOpen(false);
+
+	// うえの変数は不要
+	const [isDrawerMenuVisible, setIsDrawerMenuVisible] = useState<boolean>(false);
+	const [open , setOpen]  = useState<boolean>(true);
+
+	// どう書くのがいいのか検討
+	const handleStateChange = ():void => setOpen(true);
+	const handleClose       = ():void => setOpen(false);
+	const showDrawer        = ():void => { setIsDrawerMenuVisible(true);}; 
+	const onClose           = ():void => { setIsDrawerMenuVisible(false);};
 
 	return (
 		<nav className='header' data-test='navbar'>
@@ -27,33 +25,16 @@ const Header: React.FC = () => {
 				 {/* <Link></Link> */}
 				 <span>Firegram</span>
 			 </div>
+			 {open && <UploadModal open={open} handleClose={handleClose} />}
+			 <IconButton
+				edge="start"
+				// className={classes.menuButton}
+			    onClick={() => showDrawer()}
+				color="inherit"
+				aria-label="menu"
+             >
 			 <MenuIcon />
-			 <div className='menu__container'>
-                <div className='menu_rigth'>
-				    <RightMenu mode='inline' ata-test='menu-outside-drawer' />
-				</div>
-				{open && <UploadModal open={open} handleClose={handleClose} />}
-				<Button
-					data-test='button-show-drawer'
-					className='menu__mobile-button'
-					type='primary'
-					onClick={showDrawer}
-					>
-						button button
-					{/* <Icon type='align-left' /> */}
-				</Button>
-				<Drawer
-					data-test='drawer-menu'
-					title='Firegram'
-					placement='left'
-					className='menu_drawer'
-					closable={false}
-					onClose={onClose}
-					visible={isDrawerMenuVisible}
-					>
-				</Drawer>
-	
-			 </div>
+			 </IconButton>
 		</nav>
 	);
 };
