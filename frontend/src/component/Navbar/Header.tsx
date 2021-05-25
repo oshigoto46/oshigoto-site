f1b3997f4df2b1c995e793e785855aa6b49cae6f
shiro <ppp.shiro46@gmail.com>
2021-05-25 15:53:03 +0000

    https://github.com/oshigoto46/oshigoto-site/issues/5

--- frontend/src/component/Navbar/Header.tsx
+++ frontend/src/component/Navbar/Header.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
-
+import { slide as Menu } from "react-burger-menu";
 //import RightMenu from '../Right-Menu/RightMenu'
 // import { Link } from 'react-router-dom';
 // import { Drawer, Button} from 'antd';
 
 const Header: React.FC = () => {
+
+	const [menuOpen,setMenuOpen] = useState(false);
+	const handleStateChange = ():void => setMenuOpen(true);
 	return (
 		<nav className='header' data-test='navbar'>
 			 <div className='menu__logo'>
 				 <span>Oshigoto46 Site</span>
 			 </div>
+			 <Menu
+				isOpen={menuOpen}
+				onStateChange={state => handleStateChange()}
+			 >
+			 </Menu>
 			
 		</nav>
 	);
